6aa44178a07020bef14f3d8dd1aa8669af957b11
trangbtt <trangvtg@gmail.com>
2015-01-17 00:04:28 +0700

    trang commit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,42 @@
 'use strict';
 
 var DnDMoM = {};
+var hotEvent =undefined;
 
 jQuery(document).ready(function(e) {
-    (function($) {
 
+    (function($) {
         //init
         DnDMoM.initMainNav();
-        DnDMoM.initImageSlider('#key-features');
+        DnDMoM.initImageSlider('#key-features');     
+        
+        $(window).resize(function(event) {
+
+            if(Modernizr.mq('only screen and (max-width: 666px)')){
+                
+                if (typeof hotEvent == 'undefined') {
+                    hotEvent = DnDMoM.initHotEvent('#hot-events-list');
+                   
+                }    
+            }else {                
+                if (typeof hotEvent != 'undefined') {                   
+                    hotEvent = DnDMoM.destroySlider(hotEvent);
+                }    
+            }
+        });
+        
+        $(window).trigger("resize");
 
     })(jQuery);
+
+   
 });
 
 //functions
 DnDMoM = (function($) {
+
     return {
+
         initMainNav: function() {
             //main navigation
             $('#main-nav__list')
@@ -39,6 +61,31 @@ DnDMoM = (function($) {
                 freeMode: true,
                 freeModeFluid: true
             });
+        },
+
+
+        initHotEvent: function($selector) {
+            
+                return $( $selector ).swiper({                    
+                    paginationClickable: true,
+                    slidesPerView: 'auto',                   
+                    slideElement:'li',                   
+                    calculateHeight:true
+
+                });          
+
+        },
+
+        destroySlider:  function(object) {
+
+            if (typeof object != 'undefined') {                
+                // destroy and delete swiper object
+                var container = jQuery(object.container);
+                container.find("*").stop(true, true).removeAttr('style');     
+                object.destroy();             
+                return undefined;
+            }
         }
+
     }
 })(jQuery);
