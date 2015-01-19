4f9c1841ad9a13bba547449e5218da481708f596
KhoaNT <ukhome@gmail.com>
2015-01-19 17:08:16 +0700

    Update subpage

---
+++ php/fake-response.php
@@ -0,0 +1,13 @@
+<?php
+    print_r( json_encode(array(
+        'totalRating' => 3069,
+        'average' => 4.6,
+        'statistic' => array(
+            '1' => 769,
+            '2' => 321,
+            '3' => 457,
+            '4' => 503,
+            '5' => 1001
+        )
+    )) );
+?>
