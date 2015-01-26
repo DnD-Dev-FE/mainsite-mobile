1fde8a68ec30ed32e9e2ddbd95c24ddb7d713061
KhoaNT <ukhome@gmail.com>
2015-01-26 17:00:40 +0700

    Update dist folder

---
+++ dist/php/su-kien/danh-sach.3.php
@@ -0,0 +1,14 @@
+<?php
+    sleep(1);
+    for ( $i=0; $i < 5; $i++ ) {
+        echo('
+            <li>
+                <p><strong class="posts__cate">3. Sự kiện</strong>&nbsp;|&nbsp;<time class="posts__time">34 phút trước</time></p>
+                <a href="post-details.html" title="">
+                    <h3>Page 3: Cập nhật phiên bản mới</h3>
+                    <img src="" title="Cập nhật phiên bản mới" alt="Cập nhật phiên bản mới" />
+                </a>
+            </li>
+        ');
+    }
+?>
---
+++ dist/php/tin-tuc/danh-sach.1.php
@@ -0,0 +1,13 @@
+<?php
+    sleep(1);
+    for ( $i=0; $i < 5; $i++ ) {
+        echo('
+            <li>
+                <p><strong class="posts__cate">1. Tin tức</strong>&nbsp;|&nbsp;<time class="posts__time">34 phút trước</time></p>
+                <a href="post-details.html" title="">
+                    <h3>Page 1: Lưu ý về vấn đề nạp Xu vào 08:00 sáng 08.01.2015</h3>
+                </a>
+            </li>
+        ');
+    }
+?>
