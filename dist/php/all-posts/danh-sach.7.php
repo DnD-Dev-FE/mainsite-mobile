d49c284b02738a13f0b92cbb940094651f08d8ea
KhoaNT <ukhome@gmail.com>
2015-02-02 11:16:41 +0700

    Update

---
+++ dist/php/all-posts/danh-sach.7.php
@@ -0,0 +1,16 @@
+<?php
+    sleep(1);
+    for ( $i=0; $i < 5; $i++ ) {
+        echo('
+            <li>
+                <p><strong class="posts__cate">5. ' . ( $i%2 == 0 ? 'Sự kiện' : 'Tin tức' ) . '</strong>&nbsp;|&nbsp;<time class="posts__time">34 phút trước</time></p>
+                <a href="post-details.html" title="">
+                    <h3>Page 5: Cập nhật phiên bản mới</h3>' .
+                    ( $i%2 == 0 ? '<img src="" title="Cập nhật phiên bản mới" alt="Cập nhật phiên bản mới" />' : '' ) . '
+                </a>
+            </li>
+        ');
+    }
+?>
+<input type="hidden" id="itemTotal" value="57" />
+<input type="hidden" id="itemPerPage" value="10" />
