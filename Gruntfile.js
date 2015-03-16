b164a87c1fb35b6c57ad12665d60bde9625e451a
unknown <khoant@cpu00953.VNG.COM.VN>
2015-03-16 14:51:33 +0700

    Update 	modified:   Gruntfile.js 	modified:   css/screen.css 	modified:   css/subpage.css 	modified:   dist/css/dnd.mom.homepage.min.css 	modified:   dist/css/dnd.mom.subpage.min.css 	modified:   dist/js/dnd.mom.gallery.js 	modified:   dist/js/dnd.mom.gallery.min.js 	modified:   dist/js/dnd.mom.homepage.js 	modified:   dist/js/dnd.mom.homepage.min.js 	modified:   dist/js/dnd.mom.posts.js 	modified:   dist/js/dnd.mom.posts.min.js 	modified:   sass/screen.scss

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,6 @@ module.exports = function(grunt) {
                 src: [
                     'js/jquery2.min.js',
                     'widgets/swiper/js/idangerous.swiper.min.js',
-                    'widgets/fancybox/js/jquery.fancybox.pack.js',
                     'widgets/photoswipe/js/photoswipe.min.js',
                     'widgets/photoswipe/js/photoswipe-ui.min.js',
                     'js/modernizr.js',
@@ -49,7 +48,6 @@ module.exports = function(grunt) {
                 // the files to concatenate
                 src: [
                     'js/jquery2.min.js',
-                    'widgets/swiper/js/idangerous.swiper.min.js',
                     'js/modernizr.js',
                     'js/signals.min.js',
                     'js/hasher.min.js',
@@ -75,7 +73,6 @@ module.exports = function(grunt) {
                 // the files to concatenate
                 src: [
                     'js/jquery2.min.js',
-                    'widgets/fancybox/js/jquery.fancybox.pack.js',
                     'widgets/photoswipe/js/photoswipe.min.js',
                     'widgets/photoswipe/js/photoswipe-ui.min.js',
                     'js/modernizr.js',
